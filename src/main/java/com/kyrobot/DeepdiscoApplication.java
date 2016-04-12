c881956094f34d9c455aa34454af0c6d075e3a0f
Kyle <kylemilner@me.com>
2016-04-12 22:28:24 +0100

    add Spring boot security & OAuth2 to Spotify. Implement web security filter chain optionally protect resources, new index.html is unprotected. .properties > .yml

--- src/main/java/com/kyrobot/DeepdiscoApplication.java
+++ src/main/java/com/kyrobot/DeepdiscoApplication.java
@@ -2,9 +2,20 @@ package com.kyrobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class DeepdiscoApplication {
+@EnableOAuth2Sso
+public class DeepdiscoApplication extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests()
+			.antMatchers("/", "/login**").permitAll()
+			.anyRequest().permitAll();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeepdiscoApplication.class, args);
